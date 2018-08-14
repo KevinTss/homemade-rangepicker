88e1fd724a8400724bedaa38f8ce414ad053a37b
Kevin <kevin.tassi@gmail.com>
2018-08-14 16:10:20 +0200

    wip ranges hanlder

--- src/components/RangePicker.vue
+++ src/components/RangePicker.vue
@@ -19,7 +19,12 @@
       </div>
 
       <div class="rangepicker_month_numbers">
-        1
+        <div v-for='(day, i) in month.getDays()'
+             :key='`day-of-month-${month.getName()}-${i}`'
+             class="rangepicker_day"
+             :class="classForDay(day, month)">
+          {{ day.getDate() }}
+        </div>
       </div>
 
     </div>
@@ -28,19 +33,36 @@
 
 <script>
 import Month from './Month.js'
+import Ranges from './Ranges.js'
 
 export default {
   name: 'rangepicker',
   props: {
-    year: Number
+    year: Number,
+    value: Array
   },
   data () {
     return {
-      months: []
+      months: [],
+      ranges: []
     }
   },
   mounted () {
     this.months = Month.createMonthForYear(this.year)
+    this.ranges = Ranges.fromTimestamps(this.value)
+  },
+  methods: {
+    classForDay (day, month) {
+      let classes = []
+      console.log('aa', this.ranges)
+      // if (this.ranges.contains(day)) {
+      //   classes.push('rangepicker_range')
+      // }
+      if (!month.contains(day)) {
+        classes.push('rangepicker_out')
+      }
+      return classes
+    }
   }
 }
 </script>
